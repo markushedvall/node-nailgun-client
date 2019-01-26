f2cf69a7ee7aa161f3f778f09920f616eff5ee84
Markus Hedvall <mackanhedvall@gmail.com>
2019-01-26 20:00:07 +0100

    Use Standard for linting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
 if (require.main === module) {
-  require('./lib/node-nailgun-client-cmd.js');
+  require('./lib/node-nailgun-client-cmd.js')
 } else {
-  module.exports = require('./lib/node-nailgun-client');
+  module.exports = require('./lib/node-nailgun-client')
 }
